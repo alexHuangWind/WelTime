f7fa1c645ce7cf93f201c246682e351e9727d577
changyu <alexchyandrioid@gmail.com>
2019-05-28 14:55:53 +1200

    delete unuseful code

--- app/src/main/java/weltimetable/a2019/program3/huang/changyu/weltimetable/utils/AlertDialogsHelper.java
+++ app/src/main/java/weltimetable/a2019/program3/huang/changyu/weltimetable/utils/AlertDialogsHelper.java
@@ -13,7 +13,6 @@ import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
