15fa29b4f12d97718f1f5dd77a857d4792235bba
changyu <alexchyandrioid@gmail.com>
2019-09-09 09:41:17 +1200

    Net Framework

---
+++ app/src/main/java/weltimetable/a2019/program3/huang/changyu/weltimetable/net/httpprocessor/interfaces/ICallBack.java
@@ -0,0 +1,9 @@
+package weltimetable.a2019.program3.huang.changyu.weltimetable.net.httpprocessor.interfaces;
+
+
+public interface ICallBack {
+
+    void onSuccess(String string);
+
+    void onFailed(String string);
+}
