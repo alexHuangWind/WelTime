fdf57b1b57dbd01527976d745c0498da075365df
alexchyandroid@gmail.com <Alex1127GH>
2019-10-14 10:13:59 +1300

    comments

--- app/src/main/java/weltimetable/stp/android/huang/changyu/weltimetable/net/httprequest/manager/RequestFactory.java
+++ app/src/main/java/weltimetable/stp/android/huang/changyu/weltimetable/net/httprequest/manager/RequestFactory.java
@@ -4,10 +4,9 @@ package weltimetable.stp.android.huang.changyu.weltimetable.net.httprequest.mana
 import weltimetable.stp.android.huang.changyu.weltimetable.net.httprequest.interfaces.IRequestManager;
 
 /**
- * 该类的作用是用于返回一个IRequestManager对象，这个IRequestManager的实现类
- * 可以是使用Volley实现的http请求对象，也可以是OkHttp实现的http请求对象
- * Activity/Fragment/Presenter中，只要调用getRequestManager()方法就能得到
- * http请求的操作接口，而不用关心具体是使用什么实现的。
+ * this class is used to return a IRequestManager manager object,this IRequestManager
+ * this is a Volley request obuect, and can be OkHttp request object
+
  */
 public class RequestFactory {
     public static IRequestManager getRequestManager() {
